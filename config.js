ea78873a6f1a381065abb077314b08ac32f000c0
dan <danjenner@gmail.com>
2016-08-23 16:18:15 +0000

    added mlab production db'

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://admin:password@ds013926.mlab.com:13926/shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
